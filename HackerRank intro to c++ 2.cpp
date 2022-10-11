7745a3c6ac89581a59c7e0670af7090d70a6da25
sarvinshrivastava <41331485+sarvinshrivastava@users.noreply.github.com>
2022-10-11 20:00:16 +0530

    Update HackerRank intro to c++ 2.cpp

--- HackerRank intro to c++ 2.cpp
+++ HackerRank intro to c++ 2.cpp
@@ -9,8 +9,8 @@ using namespace std;
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
     int n, i;
-    int arr[1000];
     cin >> n;
+    int arr[n];
     //cout << n << "\n";
     for (i = 0; i < n; i++) {
         cin >> arr[i];
